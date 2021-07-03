42d25c2619a2d1d5b27d8b7ef8585912c1ac8a6c
Jordan <jchan1e@gmail.com>
2021-07-03 19:20:38 +0000

    added agentfile format clarifications

--- createAgent.sh
+++ createAgent.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Args:
+# 1 path to agentfile
+# 2 training alpha
+# 3 alpha decay ratio
+# 4 montecarlo exploration bias
+# 5+ NN layer sizes
+
 echo $2 > $1.agent
 echo $3 >> $1.agent
 echo $4 >> $1.agent
