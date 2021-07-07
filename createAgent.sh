f52628a2725a84a1e8ca21241358ca5303cedb56
jordan <jchan1e@gmail.com>
2021-07-07 16:44:43 -0600

    added mutation chance and explicitly stated layer sizes

--- createAgent.sh
+++ createAgent.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Args:
-# 1 path to agentfile
+# 1 path to agent
 # 2 training alpha
 # 3 alpha decay ratio
 # 4 montecarlo exploration bias
-# 5+ NN layer sizes
+# 5 mutation chance
+# 6+ NN layer sizes
 
 echo $2 > $1.agent
 echo $3 >> $1.agent
 echo $4 >> $1.agent
+echo $5 >> $1.agent
 echo $1.nn >> $1.agent
+echo ${@:6} >> $1.agent
 
-./createNN $1.nn ${@:5}
+./createNN $1.nn ${@:6}
